269fb6667ce127e55eb0b9e85d4f195f304022fd
ghost717 <dawid.zawadzki7717@gmail.com>
2018-07-12 02:14:34 +0200

    a few examples draw function added

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # canvas-js
 
-
+Animacja Canvas JS.
 
 ### SOURCE
 
 [1] https://github.com/ghost717/gulp-app
 
 [2] https://developer.mozilla.org/pl/docs/Web/API/Canvas_API/Tutorial/rysowanie_ksztaltow
+
+[3] http://haptic-data.com/toxiclibsjs/examples/force-directed-graph-p5
+
+[4] 
--- canvas.js
+++ canvas.js
@@ -1,4 +1,4 @@
-function draw(){
+function drawTriangle(){
     var canvas = document.getElementById('canvas');
 
     if(canvas.getContext){
@@ -12,4 +12,81 @@ function draw(){
     }
 }
 
-draw();
+//Kółka rysowane na biężąco -> setTimeout() ?
+function drawCirle(){
+    var canvas = document.getElementById('canvas');
+
+    if(canvas.getContext){
+        var ctx = canvas.getContext('2d');
+
+        ctx.beginPath();
+        ctx.arc(200,50,20,0,2*Math.PI);
+        ctx.fillStyle = "#66666685";
+    //    ctx.stroke();
+        ctx.fill();
+    } 
+}
+
+function drawSmile() {
+    var canvas = document.getElementById('canvas');
+    if (canvas.getContext){
+       var ctx = canvas.getContext('2d');
+  
+      ctx.beginPath();
+      ctx.arc(375,375,50,0,Math.PI*2,true); // Outer circle
+      ctx.moveTo(0,375);
+      ctx.arc(375,375,35,0,Math.PI,false);  // Mouth (clockwise)
+      ctx.moveTo(365,365);
+      ctx.arc(360,365,5,0,Math.PI*2,true);  // Left eye
+      ctx.moveTo(395,365);
+      ctx.arc(390,365,5,0,Math.PI*2,true);  // Right eye
+      ctx.stroke();
+    }
+}
+
+function drawCircles(){
+    var canvas = document.getElementById('canvas');
+    var sWidth = canvas.width;
+    var sHeight = canvas.height;
+
+    var cirles = 50;//nrCirles;
+    
+    if (canvas.getContext){
+        var ctx = canvas.getContext('2d');
+        
+        for(var i=0; i<cirles; i++){
+            var x = getRandomInt(0,sWidth);
+            var y = getRandomInt(0,sHeight);
+            var r = getRandomInt(0,10);
+            
+            ctx.arc(x,y,r,180,Math.PI*2,true); // Outer circle
+        //    ctx.moveTo(x,y); //line
+            
+            //ctx.arc();
+        }
+    
+        ctx.stroke();
+        ctx.beginPath();
+    }
+}
+
+drawCircles();
+// drawTriangle();
+// drawCirle();
+// drawSmile();
+
+
+/**
+ * Returns a random number between min (inclusive) and max (exclusive)
+ */
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+/**
+ * Returns a random integer between min (inclusive) and max (inclusive)
+ * Using Math.round() will give you a non-uniform distribution!
+ */
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}

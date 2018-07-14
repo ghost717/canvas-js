73fb5cdb787af933700ad441252a60f9573b682d
ghost717 <dawid.zawadzki7717@gmail.com>
2018-07-14 20:55:03 +0200

    transform function added

--- canvas.js
+++ canvas.js
@@ -238,15 +238,35 @@ function scale(){
 
     ctx.scale(-1, 1);
     ctx.font = '20px serif';
-//    ctx.fillText('MDN', -135, 120);
+    ctx.fillText('MDN', -135, 120);
+}
+
+function transforms(){
+    var ctx = document.getElementById('canvas2').getContext('2d');
+
+    var sin = Math.sin(Math.PI / 6);
+    var cos = Math.cos(Math.PI / 6);
+    ctx.translate(425, 100);
+
+    var c = 0;
+    for (var i = 0; i <= 12; i++) {
+        c = Math.floor(255 / 12 * i);
+        ctx.fillStyle = 'rgb(' + c + ', ' + c + ', ' + c + ')';
+        ctx.fillRect(0, 0, 100, 10);
+        ctx.transform(cos, sin, -sin, cos, 0, 0);
+    }
+    
+    ctx.setTransform(-1, 0, 0, 1, 400, 100);
+    ctx.fillStyle = 'rgba(255, 128, 255, 0.5)';
+    ctx.fillRect(0, 50, 100, 100);
 }
 
 drawPath();
 saveRestore();
 translate();
 //rotate();
-scale();
-
+//scale();
+transforms();
 
 //drawCircles2();
 //drawCircles();
